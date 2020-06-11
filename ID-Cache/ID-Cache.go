5ba8f89be3c4507dc2cf702904d851bd3a008562
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-06-11 12:25:27 -0500

    Crawler can now load a yt-video.db, added prompt and goroutines

--- ID-Cache/ID-Cache.go
+++ ID-Cache/ID-Cache.go
@@ -9,13 +9,15 @@ import "fmt"
 var queueCount uint64 = 0
 var bstCount uint64 = 0
 
-func Insert(id uint64) {
+func Insert(id uint64) bool {
 	c_id := C.ulong(id)
 	if uint64(C.BST_insert(c_id)) != 0 {
+		C.enqueue(c_id)
 		queueCount++
 		bstCount++
-		C.enqueue(c_id)
+		return true
 	}
+	return false
 }
 
 func TryAgainLater(id uint64) {
@@ -24,10 +26,20 @@ func TryAgainLater(id uint64) {
 }
 
 func Next() (uint64) {
-	queueCount--
-	return uint64(C.dequeue())
+	status := uint64(C.dequeue())
+	if status == 0 {
+		fmt.Println("Warning: Attempting to dequeue empty queue.")
+	} else {
+		queueCount--
+	}
+	return status
 }
 
-func status() {
-	fmt.Printf("Processed: %d, Waiting: %d, Total %d\n", bstCount - queueCount, queueCount, bstCount)
+func Status() {
+	fmt.Printf("IDs: Processed %d, Waiting %d, Total %d\n",
+	bstCount - queueCount, queueCount, bstCount)
+}
+
+func QueueCount() uint64 {
+	return queueCount
 }
--- crawler.go
+++ crawler.go
@@ -8,21 +8,44 @@ import (
     "github.com/PuerkitoBio/goquery"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"time"
 )
 
 func main() {
-	seed_ID := "hsWr_JWTZss"
-	cache.Insert(b64.Decode64(seed_ID))
 	c := make(chan []interface{})
 
+	thread_count := 1
+
+	load_db()
 	go inserter(c)
-	crawler(c)
+	for i := 0; i < thread_count; i++ {
+		go crawler(c)
+		for cache.QueueCount() < 5 {
+			time.Sleep(time.Millisecond * 100)
+		}
+	}
+
+	var input string
+	for {
+		fmt.Println("[s]tatus, [q]uite, [a]dd thread")
+		fmt.Scan(&input)
+		switch input {
+		case "a":
+			go crawler(c)
+			thread_count++
+		case "s":
+			cache.Status()
+			fmt.Printf("Threads: %d\n", thread_count)
+		case "q":
+			return
+		default:
+			fmt.Println("Invalid input")
+		}
+	}
 }
 
-func inserter(c chan []interface{}) {
+func load_db() {
 	db, err := sql.Open("sqlite3", "./yt-videos.db")
-	checkErr(err)
-
 	table_stmt, err := db.Prepare(`CREATE TABLE IF NOT EXISTS video (
 	video_id INTEGER(64) PRIMARY KEY,
 	rec_1 INTEGER(64) NOT NULL,
@@ -46,22 +69,65 @@ func inserter(c chan []interface{}) {
 	checkErr(err)
 	table_stmt.Exec()
 
-	insert_stmt, err := db.Prepare(`INSERT INTO video
+	var row_count int64
+	rows, _ := db.Query("SELECT COUNT(*) FROM video")
+	for rows.Next() {
+		rows.Scan(&row_count)
+	}
+
+	if row_count == 0 {
+		fmt.Println("yt-video.db not found. Loading seed ID hsWr_JWTZss")
+		seed_ID := "hsWr_JWTZss"
+		cache.Insert(b64.Decode64(seed_ID))
+		return
+	}
+
+	var video_id int64
+	rows, _ = db.Query("SELECT video_id FROM video")
+	for rows.Next() {
+		rows.Scan(&video_id)
+		cache.Insert(uint64(video_id))
+		cache.Next()
+	}
+
+	if cache.QueueCount() != 0 {
+		log.Fatal("Fatal: Queue count is not zero")
+	}
+
+	var rec [18]int64
+	rows2, _ := db.Query(`SELECT rec_1, rec_2, rec_3, rec_4, rec_5, rec_6, rec_7, rec_8, rec_9,
+	                    rec_10, rec_11, rec_12, rec_13, rec_14, rec_15, rec_16, rec_17, rec_18 FROM video`)
+	for rows2.Next() {
+		rows2.Scan(&rec[0], &rec[1], &rec[2], &rec[3], &rec[4], &rec[5], &rec[6], &rec[7], &rec[8],
+		&rec[9], &rec[10], &rec[11], &rec[12], &rec[13], &rec[14], &rec[15], &rec[16], &rec[17])
+		for i := 0; i < 18; i++ {
+			cache.Insert(uint64(rec[i]))
+		}
+	}
+	cache.Status()
+	db.Close()
+}
+
+func inserter(c chan []interface{}) {
+	db, err := sql.Open("sqlite3", "./yt-videos.db")
+	stmt, err := db.Prepare(`INSERT INTO video
 	(video_id, rec_1, rec_2, rec_3, rec_4, rec_5, rec_6, rec_7, rec_8, rec_9,
 	rec_10, rec_11, rec_12, rec_13, rec_14, rec_15, rec_16, rec_17, rec_18)
 	values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`)
 	checkErr(err)
 
 	for {
-		_, err := insert_stmt.Exec(<-c...)
+		_, err := stmt.Exec(<-c...)
 		checkErr(err)
 	}
+	db.Close()
 }
 
 func crawler(c chan []interface{}) {
 	for id := cache.Next(); id != 0; id = cache.Next() {
 		var	rec_count int
-		row := []interface{}{int64(id)} // This will be inserted into yt-videos.db
+		row := []interface{}{} // This will be inserted into yt-videos.db
+		row = append(row, int64(id))
 		doc, err := goquery.NewDocument("https://www.youtube.com/watch?v=" + b64.Encode64(id))
 		checkErr(err)
 		rec_sel := doc.Find(".content-link.spf-link")
@@ -73,6 +139,7 @@ func crawler(c chan []interface{}) {
 			 cache.TryAgainLater(id)
 			 continue
 		}
+
 		rec_sel.EachWithBreak(func(index int, item *goquery.Selection) bool {
 			link, err := item.Attr("href")
 			if err == false {
