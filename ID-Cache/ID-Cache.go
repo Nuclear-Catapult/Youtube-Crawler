c37e9ac73b0e48d076ce85afa55b7f8b404695d0
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-06-12 04:51:46 -0500

    I Replaced the (non-AVL) binary search tree with a hash table. Now it won't hurt to load ordered IDs

--- ID-Cache/ID-Cache.go
+++ ID-Cache/ID-Cache.go
@@ -1,21 +1,21 @@
 package idcache
 
 // #cgo LDFLAGS: -L${SRCDIR} -lcache -pthread
-// #include "bst.h"
+// #include "hash_table.h"
 // #include "queue.h"
 import "C"
 import "fmt"
 import "log"
 
 var queueCount int64 = 0
-var bstCount int64 = 0
+var tableCount int64 = 0
 
 func Insert(id int64) bool {
 	c_id := C.long(id)
-	if int64(C.BST_insert(c_id)) != 0 {
+	if int64(C.table_insert(c_id)) != 0 {
 		C.enqueue(c_id)
 		queueCount++
-		bstCount++
+		tableCount++
 		return true
 	}
 	return false
@@ -38,14 +38,15 @@ func Next() (int64) {
 
 func Key_Insert(id int64) {
 	c_id := C.long(id)
-	if int64(C.BST_insert(c_id)) == 0 {
+	if int64(C.table_insert(c_id)) == 0 {
 		log.Fatal("Fatal: Loading the primary keys resulted in duplicate values.")
 	}
+	tableCount++
 }
 
 func Status() {
 	fmt.Printf("IDs: Processed %d, Waiting %d, Total %d\n",
-	bstCount - queueCount, queueCount, bstCount)
+	tableCount - queueCount, queueCount, tableCount)
 }
 
 func QueueCount() int64 {
